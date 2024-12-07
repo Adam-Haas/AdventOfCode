29b254a022d2620d6a7094c79970c9181f552a10
Adam Haas <adam@adamhaas.co.uk>
2024-12-07 19:02:03 +0000

    Day 1 solution - part 2

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let locations1 = vec![3, 4, 2, 1, 3, 3];
     let locations2 = vec![4, 3, 5, 3, 9, 3];
 
-    let solution = solve(locations1, locations2);
+    let solution = solve_pt2(locations1, locations2);
     println!("{}", solution)
 }
 
@@ -19,6 +19,32 @@ fn solve(mut locs1: Vec<i32>, mut locs2: Vec<i32>) -> i32 {
     while i < locs1.len() {
         ret_val += (locs1[i] - locs2[i]).abs();
         i+=1;
+    }
+
+    ret_val
+}
+
+fn solve_pt2(mut locs1: Vec<i32>, mut locs2: Vec<i32>) -> i32 {
+    if locs1.len() != locs2.len() {
+        panic!("locations vectors must be of equal length")
+    }
+
+    // locs1.sort();
+    locs2.sort();
+
+    let mut ret_val = 0;
+    for elem in locs1 {
+        let mut count_in_locs_2 = 0;
+        for elem_l2 in &locs2 {
+            if elem == *elem_l2 {
+                count_in_locs_2 += 1;
+            }
+            if elem < *elem_l2 {
+                break
+            }
+        }
+
+        ret_val += elem * count_in_locs_2;
     }
 
     ret_val
